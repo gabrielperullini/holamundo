2895b895db9625a10534f22195af232e29b58f27
gabrielperullini <gabrielperullini@hotmail.com>
2023-06-28 21:43:26 -0300

    Update build.groovy

--- build.groovy
+++ build.groovy
@@ -37,7 +37,7 @@ pipeline{
             steps{
                 sh '''
                     #mvn clean install
-                    /home/jenkins/apache-maven-3.6.3/bin/mvn package
+                    /usr/bin/mvn package
                 '''
             }
         }
