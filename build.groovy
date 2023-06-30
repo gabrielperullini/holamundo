79b27e411776caab03e45496a730bc9eb8d73c63
gabrielperullini <gabrielperullini@hotmail.com>
2023-06-29 21:10:38 -0300

    Update build.groovy6

--- build.groovy
+++ build.groovy
@@ -37,7 +37,7 @@ pipeline{
             steps{
                 sh '''
                     #mvn clean install
-                    /usr/bin/mvn/mvn package
+                    /usr/bin/mvn package
                 '''
             }
         }
